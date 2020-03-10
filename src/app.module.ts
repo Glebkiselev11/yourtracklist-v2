8fc02fb79154e5749d08f93b06082b2426d12ea5
glebkiselev11 <12bny23gleb@gmail.com>
2020-03-10 12:55:08 +0300

    added http-error handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthorsModule } from './authors/authors.module';
 import { ReleasesModule } from './releases/releases.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
 
 @Module({
   imports: [
@@ -10,6 +12,12 @@ import { ReleasesModule } from './releases/releases.module';
     AuthorsModule, 
     ReleasesModule, 
     TypeOrmModule.forRoot()
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter
+    }
   ]
 })
 export class AppModule {}
---
+++ src/shared/http-error.filter.ts
@@ -0,0 +1,24 @@
+import { Catch, ExceptionFilter, HttpException, ArgumentsHost, Logger } from '@nestjs/common'
+
+
+@Catch()
+export class HttpErrorFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const req = ctx.getRequest()
+    const res = ctx.getResponse()
+    const status = exception.getStatus()
+
+    const errorResponse = {
+      code: status,
+      timestamp: new Date().toUTCString(),
+      path: req.url,
+      method: req.method,
+      message: exception.message?.error || null,
+    }
+
+    Logger.error(`${req.method} ${req.url}`, JSON.stringify(errorResponse), 'ExceptionFilter')
+
+    res.status(status).json(errorResponse)
+  }
+}
